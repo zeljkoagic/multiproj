1a9fea67c6bdb90df659084ef431a510187858b7
Zeljko Agic <zeljko.agic@gmail.com>
2015-10-26 13:35:22 +0100

    updated sd normalization

--- tools/projection/normalize_sd.py
+++ tools/projection/normalize_sd.py
@@ -11,23 +11,22 @@ def clip(x):
         return 1.0
     else:
         return x
+
 def chunkIt(seq, num):
-  avg = len(seq) / float(num)
-  out = []
-  last = 0.0
-  while last < len(seq):
-    out.append(seq[int(last):int(last + avg)])
-    last += avg
-  return out
+    avg = len(seq) / float(num)
+    out = []
+    last = 0.0
+    while last < len(seq):
+        out.append(seq[int(last):int(last + avg)])
+        last += avg
+    return out
 
 def rank(X):
     y=[str((i+1)/len(X)) for i in range(len(X))]
     return dict(zip(sorted(X),y))
-
 def intrank(X):
     y=[str((i+1)) for i in range(len(X))]
     return dict(zip(sorted(X),y))
-
 def quantilize(X):
     D={}
     SD=np.std(X)
@@ -40,14 +39,13 @@ def quantilize(X):
             if D_prime[x] in chunk:
                 D[x]=str(np.array(chunk).mean())
     return dict(D)
-
 def normalize(X):
     SD=np.std(X)
     M=np.mean(X)
     D=dict(zip(
-            X,[str(clip(0.5 + float(x) - M / (2*SD))) for x in X]
-           ))
-    return dict(D) 
+        X,[str(clip(0.5 + float(x) - M / (2*SD))) for x in X]
+    ))
+    return dict(D)
 
 conll = [l.strip().split() for l in open(sys.argv[1]).readlines()]
 
@@ -56,11 +54,27 @@ sent = []
 for c in conll:
     if len(c) < 2:
         for w in sent:
-            numbers=[float(x.split(":")[1]) for x in w[8:]]
-            #D=normalize(numbers)
-            D=intrank(numbers)
-            #D=quantilize(numbers)
-            print("\t".join(w[:8] + [str(i) + ":" + D[numbers[i]] for i in range(len(numbers))])) 
+            numbers=[(int(x.split(":")[0]),float(x.split(":")[1])) for x in w[8:]]
+            numbers=dict(numbers)
+            numbers_list=[(float(x.split(":")[1])) for x in w[8:]]
+            D=normalize(numbers_list)
+            D=rank(numbers_list)
+            D=quantilize(numbers_list)
+            #            c=-1
+            #            for i in range(len(sent)+1):
+            #                if i in numbers:
+            #                    c+=1
+            #                    new_numbers[n]=D
+            string="\t".join(w[:8])+'\t'
+            weights=[]
+            for i in range(len(sent)):
+                if i in numbers:
+                    weights.append(str(i) + ":" + D[numbers[i]])
+                else:
+                    weights.append(str(i) + ":0.0")
+            string+="\t".join(weights)
+            print(string)
         sent = []
+        print()
     else:
         sent.append(c)
